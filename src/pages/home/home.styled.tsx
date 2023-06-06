5b55dc11af8f701ac0c93e83c3472ba2ab27bea8
dhanrajMohan <dhanrajmohan23@gmail.com>
2023-06-06 07:57:37 +0530

    Notes input field implemented

--- src/pages/home/home.styled.tsx
+++ src/pages/home/home.styled.tsx
@@ -1,9 +1,22 @@
-import { styled } from "@mui/material";
+import { Input, styled } from "@mui/material";
 import { Box } from "@mui/material";
 
 export namespace S {
   export const MainContainer = styled(Box)(() => ({
     background: "lavender",
     width: "100%",
+    padding: "2rem",
+  }));
+
+  export const InputContainer = styled(Box)(() => ({
+    background: "lavender",
+    width: "100%",
+  }));
+
+  export const CustomInput = styled(Input)(() => ({
+    width: "100%",
+    background: "#ffffff",
+    padding: "1rem",
+    borderRadius: "0.3rem",
   }));
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,16 @@
 import { S } from "./home.styled";
 
 export const Home = () => {
-  return <S.MainContainer>Home</S.MainContainer>;
+  return (
+    <S.MainContainer>
+      <S.InputContainer>
+        <S.CustomInput
+          id="filled-basic"
+          disableUnderline
+          placeholder="Take a note"
+          multiline
+        />
+      </S.InputContainer>
+    </S.MainContainer>
+  );
 };
